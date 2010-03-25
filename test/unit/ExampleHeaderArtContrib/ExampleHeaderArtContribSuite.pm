7fc1b192f06928e9f32e366308b33efac3148325
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:51:12 +0000

    Item8765: Replace use base with use; our @ISA;
    git-svn-id: http://svn.foswiki.org/trunk/ExampleHeaderArtContrib@6912 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/ExampleHeaderArtContrib/ExampleHeaderArtContribSuite.pm
+++ test/unit/ExampleHeaderArtContrib/ExampleHeaderArtContribSuite.pm
@@ -1,6 +1,7 @@
 package ExampleHeaderArtContribSuite;
 
-use base qw(Unit::TestSuite);
+use Unit::TestSuite;
+our @ISA = qw( Unit::TestSuite );
 
 sub name { 'ExampleHeaderArtContribSuite' };
 
--- test/unit/ExampleHeaderArtContrib/ExampleHeaderArtContribTests.pm
+++ test/unit/ExampleHeaderArtContrib/ExampleHeaderArtContribTests.pm
@@ -2,7 +2,8 @@ use strict;
 
 package ExampleHeaderArtContribTests;
 
-use base qw(FoswikiTestCase);
+use FoswikiTestCase;
+our @ISA = qw( FoswikiTestCase );
 
 use strict;
 use TWiki;
